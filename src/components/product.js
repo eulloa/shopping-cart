37f531e2a998f11fc985b4d9c28ff0edc7251f6a
Efren Ulloa <eulloa345@gmail.com>
2017-08-29 21:29:06 -0500

    Fixing small cart message bug

--- src/components/product.js
+++ src/components/product.js
@@ -11,7 +11,7 @@ const Product = (props) => {
 					<h1>{props.product.name}</h1>
 					<h2><sup>$</sup>{props.product.price}</h2>
 					<h3>{props.product.description}</h3>
-					<button className="button" onClick={() => { props.addToCart(props.product); props.setModalVisibility(true) }}>Add to cart</button>
+					<button className="button" onClick={() => { props.addToCart(props.product); props.setModalVisibility(true); props.updateCartMessage('Added to cart!') }}>Add to cart</button>
 					<Link className="button margin" to={'/product/' + props.product.productID}>Learn more</Link>
 				</figcaption>
 			</figure>
