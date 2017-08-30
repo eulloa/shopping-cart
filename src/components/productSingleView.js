43b726eaa7839ff452aa88c77027db2f483b595a
Efren Ulloa <eulloa345@gmail.com>
2017-08-30 03:08:41 -0500

    Restyle single product view and add img swap functionality

--- src/components/productSingleView.js
+++ src/components/productSingleView.js
@@ -1,42 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductSingleView = (props) => {
-	let product = props.allProducts.find((p) => p.productID === parseInt(props.match.params.productID, 10))
+class ProductSingleView extends React.Component {
+	constructor(props) {
+		super(props);
 
-	if (product == null) {
-		product = props.allProducts.find((p) => p.productID === 1)
+		this.state = {
+			currentImg: ''
+		}
+
+		this.updateImage = this.updateImage.bind(this);
+		this.getProduct = this.getProduct.bind(this);
 	}
 
-	return (
-		<div className="product single-view">
-			{ product.images.length > 1 &&
-				<ul className="images child">
-					{
-						product.images.map((img, i) => {
-							return <li key={i}><img src={'/img/' + img} alt={product.description} /></li>
-						})
-					}
-				</ul>
-			}
-			<figure className="child">
-				<h4>{product.category}</h4>
-				<img src={'/img/' + product.images[0]} alt={product.description}/>
-				<figcaption>
-					<h1>{product.name}</h1>
-					<h2><sup>$</sup>{product.price}</h2>
-					<h3>{product.description}</h3>
-					<ul>
-						{product.features.map((feature, i) => {
-							return <li key={i}>{feature}</li>
-						})}
+	updateImage = (img) => {
+		this.setState({
+			currentImg: img
+		})
+	}
+
+	getProduct = () => {
+		let product = this.props.allProducts.find((p) => p.productID === parseInt(this.props.match.params.productID, 10))
+		
+		if (product == null) {
+			product = this.props.allProducts.find((p) => p.productID === 1)
+		}
+
+		return product;
+	}
+
+	componentWillMount() {
+		this.setState({
+			currentImg: this.getProduct().images[0]
+		})
+	}
+
+	render() {
+		let product = this.getProduct();
+
+		return (
+			<div className="product single-view">
+				{ product.images.length > 1 &&
+					<ul className="images child">
+						{
+							product.images.map((img, i) => {
+								return <li key={i}><img onMouseOver={() => { this.updateImage(img) }} src={'/img/' + img} alt={product.description} /></li>
+							})
+						}
 					</ul>
-					<button className="button" onClick={() => { props.addToCart(product); props.setModalVisibility(true) }}>Add to cart</button>
-					<Link to='/'>View All</Link>
-				</figcaption>
-			</figure>
-		</div>
-	)
+				}
+				<section className="child">
+					<h4>{product.category}</h4>
+					<figure>
+						<img src={'/img/' + this.state.currentImg} />
+					</figure>
+					<div>
+						<h1>{product.name}</h1>
+						<h2><sup>$</sup>{product.price}</h2>
+						<h3>{product.description}</h3>
+						<ul>
+							{product.features.map((feature, i) => {
+								return <li key={i}>{feature}</li>
+							})}
+						</ul>
+						<button className="button" onClick={() => { this.props.addToCart(product); this.props.setModalVisibility(true) }}>Add to cart</button>
+						<Link className="button" to='/'>Back</Link>
+					</div>
+				</section>
+			</div>
+		)
+	}
 }
 
 export default ProductSingleView
