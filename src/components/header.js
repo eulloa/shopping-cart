c5b2a0e54d17cb0b093d531c049c24a1a990f264
Efren Ulloa <eulloa345@gmail.com>
2017-09-08 17:39:51 -0500

    Styling header dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -59,13 +59,15 @@ class Header extends React.Component {
 				<nav className={this.state.isSticky ? 'sticky' : ''}>
 					<h1><Link to="/">home</Link></h1>
 					<form>
-						<select onChange={this.handleOnChange}>
-							<option value="all">all</option>
-							<option value="books">books</option>
-							<option value="kitchen">kitchen</option>
-							<option value="music">music</option>
-							<option value="technology">technology</option>
-						</select>
+						<label>
+							<select onChange={this.handleOnChange}>
+								<option value="all">all</option>
+								<option value="books">books</option>
+								<option value="kitchen">kitchen</option>
+								<option value="music">music</option>
+								<option value="technology">technology</option>
+							</select>
+						</label>
 					</form>
 					<Link to="/cart"><h2><span>{this.props.shoppingCart.length}</span></h2></Link>
 				</nav>
